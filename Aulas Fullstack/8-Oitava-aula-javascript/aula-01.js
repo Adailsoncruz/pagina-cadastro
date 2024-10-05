f2bfc369ae33c7d178b5ccf8671842444d1fe94d
unknown <ADAILSON0213@DIGITALCOLLEGE.LOCAL>
2024-10-05 17:04:14 -0300

    inicio javascript

---
+++ Aulas Fullstack/8-Oitava-aula-javascript/aula-01.js
@@ -0,0 +1,15 @@
+
+const pintor = 'Gabriel'
+const altura = 3;
+const largura = 5;
+const valorPorMetro = 10;
+
+const area = largura * altura
+const calculo = area * valorPorMetro
+
+const pagamento = `O ${pintor} vai cobrar o valor de ${calculo} pelo serviço`
+
+console.log(pagamento)
+
+
+
